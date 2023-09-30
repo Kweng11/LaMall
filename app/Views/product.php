77a7472ca3435a1b9d6c2737adb3c988009298e3
Kweng11 <144525375+Kweng11@users.noreply.github.com>
2023-09-30 23:12:37 +0800

    index

---
+++ app/Views/product.php
@@ -0,0 +1,7 @@
+<?= $this-> include('/inc/top')?>
+<body>
+<?= $this-> include('/inc/navbar')?>
+<?= $this-> include('/inc/top-shop')?>
+<?= $this-> include('/inc/ourproducts')?>
+<?= $this-> include('/inc/footer')?>
+</body>
